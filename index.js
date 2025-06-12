27a7372e4f2e83a57eb2c7d0bc0b555f1c9a9185
MorrisKM <itsmorrismunene@gmail.com>
2025-06-12 13:00:00 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(name => name.toLowerCase() === str.toLowerCase());
+}
+function fuzzyMatch(arr, str) {
+    const regex = new RegExp(`^${str}`, "i");
+    return arr.filter(name => regex.test(name))
+}
+function matchName(arr, str) {
+    return arr.filter(driver => driver.name === str)
+}
